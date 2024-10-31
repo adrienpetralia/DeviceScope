2f0723857ad36cd1af05ac7c6ab510e9a1ab6c6a
Adrien Petralia <adrien.petralia@gmail.com>
2024-10-31 09:30:51 +0100

    Update

--- Pages/Benchmark.py
+++ Pages/Benchmark.py
@@ -10,34 +10,14 @@ from constants import *
 
 from utils import *
 
-st.markdown(text_tab_benchmark)
 
-col1, col2 = st.columns(2)
-
-with col1:
-    measure = st.selectbox(
-        "Choose metric", measures_list, index=0
+tab_playground, tab_benchmark, tab_about = st.tabs(
+        ["Playground", "Benchmark", "About"]
     )
-with col2:
-    dataset = st.selectbox(
-        "Choose dataset", dataset_list, index=0
-    )
+    
+with tab_playground:
+    run_metric_frame()
 
-#st.markdown("#### Overall results")
+with tab_benchmark:
+    run_nilmmodel_frame()
 
-fig1 = plot_benchmark_figures1(measure, dataset)
-fig2 = plot_benchmark_figures2(measure, dataset)
-fig3 = plot_benchmark_figures3(measure, dataset)
-
-st.plotly_chart(fig1, use_container_width=True)
-st.plotly_chart(fig2, use_container_width=True)
-st.plotly_chart(fig3, use_container_width=True)
-
-#st.markdown("#### Explore the influence of the sampling rate on the detection performance for selected appliance(s).")
-
-# appliances2 = st.multiselect(
-#     "Select devices:", devices_list_refit_ukdale,
-# )
-
-# fig_benchmark = plot_benchmark_figures4(appliances2, measure, dataset)
-# st.plotly_chart(fig_benchmark, use_container_width=True)
--- utils.py
+++ utils.py
@@ -22,6 +22,45 @@ from Models.Classifiers.ResNet5LN import ResNet5LN
 from Helpers.class_activation_map import CAM
         
 
+def run_metric_frame():
+    st.markdown(text_tab_benchmark)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        measure = st.selectbox(
+        "Choose metric", measures_list, index=0
+    )
+    with col2:
+        dataset = st.selectbox(
+        "Choose dataset", dataset_list, index=0
+    )
+
+    #st.markdown("#### Overall results")
+
+    fig1 = plot_benchmark_figures1(measure, dataset)
+    fig2 = plot_benchmark_figures2(measure, dataset)
+    fig3 = plot_benchmark_figures3(measure, dataset)
+
+    st.plotly_chart(fig1, use_container_width=True)
+    st.plotly_chart(fig2, use_container_width=True)
+    st.plotly_chart(fig3, use_container_width=True)
+
+    #st.markdown("#### Explore the influence of the sampling rate on the detection performance for selected appliance(s).")
+
+    # appliances2 = st.multiselect(
+    #     "Select devices:", devices_list_refit_ukdale,
+    # )
+
+    # fig_benchmark = plot_benchmark_figures4(appliances2, measure, dataset)
+    # st.plotly_chart(fig_benchmark, use_container_width=True)
+
+
+def run_nilmmodel_frame():
+     st.markdown("To")
+
+     
+
 def plot_benchmark_figures1(name_measure, dataset):
     table = pd.read_csv(os.getcwd()+'/TableResults/Results.gzip', compression='gzip')
     if dataset != 'All':
